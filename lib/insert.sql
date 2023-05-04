ec32b565e0ec58e26fe70262b583a28c61a38250
rknm-cell <racknahm@gmail.com>
2023-05-04 16:09:15 -0400

    completed tests

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Jeffy", 15, "M", "dark brown", "snooty", 0),
+(2,"Carl", 1, "M", "black", "intense", 1),
+(3,"Randolfo", 6, "M", "dark brown", "Nice", 1),
+(4,"Tina", 25, "F", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Baby Grinch", 27, "M", "Green", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
